6d10edda21d4cffde45547b67adf219b4fb40c17
Niiimaru <85466271+Niiimaru@users.noreply.github.com>
2021-07-15 22:11:32 +0900

    内容確認画面　更新

--- toiawase/check.php
+++ toiawase/check.php
@@ -28,7 +28,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>入力内容確認</title>
-    <link rel="stylesheet" href="check.css">
+    <link rel="stylesheet" href="../css/check.css">
 </head>
 <body>
     <h1>お問い合わせ内容</h1>
